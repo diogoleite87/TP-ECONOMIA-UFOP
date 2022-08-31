d5de65daac3f1b031f8af462f9089a7f28ece8c3
Diogo Leite <diogoleite87@gmail.com>
2022-08-31 14:04:44 -0300

    update: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,16 @@
+import * as S from './style'
+
 import NavBar from "../NavBar"
 
 export default function Header() {
 
     return (
-        <NavBar />
+        <S.Container>
+            <S.Title>Trabalho Prático - ENP150 - ECONOMIA</S.Title>
+            <S.Description href='#' target="_blank">André Lucas Santos - 20.2.8106</S.Description>
+            <S.Description href='https://github.com/diogoleite87' target="_blank">Diogo Leite Lucas - 20.2.8072</S.Description>
+            <S.Description href='https://github.com/MarqueVitor' target="_blank">Vitor Marques dos Santos - 20.2.8175</S.Description>
+            <NavBar />
+        </S.Container>
     )
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,1 +1,27 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+`
+
+export const Title = styled.h1 `
+    margin-top: 3vh;
+    margin-bottom: 2vh;
+    color: #fff;
+    text-transform: uppercase;
+
+`
+export const Description = styled.a`
+    padding: 0.5vh;
+    text-decoration: none;
+    color: #007bb6;
+    font-size: 1.2em;
+
+    &:hover {
+        color: rgb(51, 158, 83);
+        transition: 0.2s;
+    }
+`
