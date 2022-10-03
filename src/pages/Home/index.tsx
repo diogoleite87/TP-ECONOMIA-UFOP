7532f06eff90e24c24536af634236ec33e1f7d43
Diogo Leite <diogoleite87@gmail.com>
2022-10-03 14:56:57 -0300

    update: text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,42 +4,18 @@ export default function Home() {
 
     return (
         <Container title="Tela Inicial">
-            <>
-                <h2>Teste</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, officia quas in corrupti vero delectus.
-                    Fugiat cumque neque aperiam. Recusandae cupiditate animi nam impedit eius consequatur id commodi
-                    minus quaerat?</p>
+            <h2>Motivação do trabalho desenvolvido</h2>
+            <p>O site foi desenvolvido durante a disciplina de Economia, do curso Sistemas de Informações - UFOP. Com intuito de botar em prática um pouco do que foi aprendido/apresentado durante sala de aula.</p>
 
-                <h2>Teste</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, officia quas in corrupti vero delectus.
-                    Fugiat cumque neque aperiam. Recusandae cupiditate animi nam impedit eius consequatur id commodi
-                    minus quaerat?</p>
+            <h2>Assunto tratado no trabalho</h2>
+            <p>O presente trabalho aborda alguns conceitos da Economia, entre eles é apresentado Custo de Produção e Curva de Possibilidade de Produção. Ambos com suas respectivas páginas, contendo as informações e uma calculadora, seguida de gráficos.</p>
 
-                <h2>Teste</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, officia quas in corrupti vero delectus.
-                    Fugiat cumque neque aperiam. Recusandae cupiditate animi nam impedit eius consequatur id commodi
-                    minus quaerat?</p>
+            <h2>Linguagem e tecnologia na qual foi construído</h2>
+            <p>O site foi construído em React e TypeScript, em seu formato Web.</p>
 
-                <h2>Teste</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, officia quas in corrupti vero delectus.
-                    Fugiat cumque neque aperiam. Recusandae cupiditate animi nam impedit eius consequatur id commodi
-                    minus quaerat?</p>
+            <h2>Integrantes do grupo</h2>
+            <p>André Lucas Santos, Diogo Leite Lucas e Vitor Marques dos Santos.</p>
 
-                <h2>Teste</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, officia quas in corrupti vero delectus.
-                    Fugiat cumque neque aperiam. Recusandae cupiditate animi nam impedit eius consequatur id commodi
-                    minus quaerat?</p>
-
-                <h2>Teste</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, officia quas in corrupti vero delectus.
-                    Fugiat cumque neque aperiam. Recusandae cupiditate animi nam impedit eius consequatur id commodi
-                    minus quaerat?</p>
-
-                <h2>Teste</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, officia quas in corrupti vero delectus.
-                    Fugiat cumque neque aperiam. Recusandae cupiditate animi nam impedit eius consequatur id commodi
-                    minus quaerat?</p>
-            </>
         </Container>
     )
 }
