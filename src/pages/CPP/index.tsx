383e5359d8af259021020840dc13d125fc8e5ddc
Diogo Leite <diogoleite87@gmail.com>
2022-10-03 14:56:39 -0300

    update: text

--- src/pages/CPP/index.tsx
+++ src/pages/CPP/index.tsx
@@ -6,10 +6,12 @@ export default function CPP() {
     return (
         <Container title="TABELA">
             <>
-                <h2>Teste</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Voluptatum, id reprehenderit eius modi doloribus provident atque.
-                    Odio voluptate qui cupiditate quam voluptatibus quasi delectus itaque quae non fuga, cum placeat!</p>
+                <h2>CPP (Curva de Possibilidade de Produção)</h2>
+                <p>A Curva de Possibilidade de Produção (CPP) é uma ferramenta que demonstra, em formato gráfico, a capacidade de produção de um determinado produto. Este recurso pode ser utilizado, inclusive, para comparar o desempenho de dois produtos diferentes. A economia trabalha com três questões básicas: O que produzir, para quem produzir e como produzir. Quando se trata de CPP os principais valores analisados são o que produzir e como produzir. É preciso ter em mente que a CPP é utilizada, em geral, para comparar a capacidade produtiva de dois produtos diferentes.</p>
+                <h2>Vantagens de se utilizar a CPP</h2>
+                <p>A CPP é vista como básica porque, por meio dela, é possível considerar os fatores de produção para chegar à capacidade produtiva da empresa, cidade ou país.
+                    A eficiência produtiva será o fator determinante sobre a possibilidade de investir em determinado tipo de produto ou não.
+                </p>
                 <CppGraphic />
             </>
         </Container>
